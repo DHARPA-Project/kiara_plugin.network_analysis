384d6ceeee5efce34ecd696e83334a38b26c70fb
Markus Binsteiner <markus@frkl.io>
2025-06-14 13:28:03 +0200

    chore: copy old version of data type

--- src/kiara_plugin/network_analysis/data_types.py
+++ src/kiara_plugin/network_analysis/data_types.py
@@ -1,3 +1,257 @@
 # -*- coding: utf-8 -*-
 
 """This module contains the value type classes that are used in the ``kiara_plugin.network_analysis`` package."""
+
+# -*- coding: utf-8 -*-
+
+"""This module contains the value type classes that are used in the ``kiara_plugin.network_analysis`` package.
+"""
+from typing import Any, ClassVar, List, Mapping, Type, Union
+
+from rich.console import Group
+
+from kiara.defaults import DEFAULT_PRETTY_PRINT_CONFIG
+from kiara.exceptions import KiaraException
+from kiara.models.values.value import Value
+from kiara.utils.output import ArrowTabularWrap
+from kiara_plugin.network_analysis.defaults import (
+    CONNECTIONS_COLUMN_NAME,
+    CONNECTIONS_MULTI_COLUMN_NAME,
+    COUNT_DIRECTED_COLUMN_NAME,
+    COUNT_IDX_DIRECTED_COLUMN_NAME,
+    COUNT_IDX_UNDIRECTED_COLUMN_NAME,
+    COUNT_UNDIRECTED_COLUMN_NAME,
+    EDGE_ID_COLUMN_NAME,
+    EDGES_TABLE_NAME,
+    IN_DIRECTED_COLUMN_NAME,
+    IN_DIRECTED_MULTI_COLUMN_NAME,
+    LABEL_COLUMN_NAME,
+    NODE_ID_COLUMN_NAME,
+    NODES_TABLE_NAME,
+    OUT_DIRECTED_COLUMN_NAME,
+    OUT_DIRECTED_MULTI_COLUMN_NAME,
+    SOURCE_COLUMN_NAME,
+    TARGET_COLUMN_NAME,
+)
+from kiara_plugin.network_analysis.models import NetworkData
+from kiara_plugin.tabular.data_types.tables import TablesType
+from kiara_plugin.tabular.models.tables import KiaraTables
+
+
+class NetworkDataType(TablesType):
+    """Data that can be assembled into a graph.
+
+    This data type extends the 'tables' type from the [kiara_plugin.tabular](https://github.com/DHARPA-Project/kiara_plugin.tabular) plugin, restricting the allowed tables to one called 'edges',
+    and one called 'nodes'.
+    """
+
+    _data_type_name: ClassVar[str] = "network_data"
+    _cached_doc: ClassVar[Union[str, None]] = None
+
+    @classmethod
+    def python_class(cls) -> Type:
+        return NetworkData  # type: ignore
+
+    @classmethod
+    def type_doc(cls) -> str:
+
+        if cls._cached_doc:
+            return cls._cached_doc
+
+        from kiara_plugin.network_analysis.models.metadata import (
+            EDGE_COUNT_DUP_DIRECTED_COLUMN_METADATA,
+            EDGE_COUNT_DUP_UNDIRECTED_COLUMN_METADATA,
+            EDGE_ID_COLUMN_METADATA,
+            EDGE_IDX_DUP_DIRECTED_COLUMN_METADATA,
+            EDGE_IDX_DUP_UNDIRECTED_COLUMN_METADATA,
+            EDGE_SOURCE_COLUMN_METADATA,
+            EDGE_TARGET_COLUMN_METADATA,
+            NODE_COUND_EDGES_MULTI_COLUMN_METADATA,
+            NODE_COUNT_EDGES_COLUMN_METADATA,
+            NODE_COUNT_IN_EDGES_COLUMN_METADATA,
+            NODE_COUNT_IN_EDGES_MULTI_COLUMN_METADATA,
+            NODE_COUNT_OUT_EDGES_COLUMN_METADATA,
+            NODE_COUNT_OUT_EDGES_MULTI_COLUMN_METADATA,
+            NODE_ID_COLUMN_METADATA,
+            NODE_LABEL_COLUMN_METADATA,
+        )
+
+        edge_properties = {}
+        edge_properties[EDGE_ID_COLUMN_NAME] = EDGE_ID_COLUMN_METADATA.doc.full_doc
+        edge_properties[SOURCE_COLUMN_NAME] = EDGE_SOURCE_COLUMN_METADATA.doc.full_doc
+        edge_properties[TARGET_COLUMN_NAME] = EDGE_TARGET_COLUMN_METADATA.doc.full_doc
+        edge_properties[
+            COUNT_DIRECTED_COLUMN_NAME
+        ] = EDGE_COUNT_DUP_DIRECTED_COLUMN_METADATA.doc.full_doc
+        edge_properties[
+            COUNT_IDX_DIRECTED_COLUMN_NAME
+        ] = EDGE_IDX_DUP_DIRECTED_COLUMN_METADATA.doc.full_doc
+        edge_properties[
+            COUNT_UNDIRECTED_COLUMN_NAME
+        ] = EDGE_COUNT_DUP_UNDIRECTED_COLUMN_METADATA.doc.full_doc
+        edge_properties[
+            COUNT_IDX_UNDIRECTED_COLUMN_NAME
+        ] = EDGE_IDX_DUP_UNDIRECTED_COLUMN_METADATA.doc.full_doc
+
+        properties_node = {}
+        properties_node[NODE_ID_COLUMN_NAME] = NODE_ID_COLUMN_METADATA.doc.full_doc
+        properties_node[LABEL_COLUMN_NAME] = NODE_LABEL_COLUMN_METADATA.doc.full_doc
+        properties_node[
+            CONNECTIONS_COLUMN_NAME
+        ] = NODE_COUNT_EDGES_COLUMN_METADATA.doc.full_doc
+        properties_node[
+            CONNECTIONS_MULTI_COLUMN_NAME
+        ] = NODE_COUND_EDGES_MULTI_COLUMN_METADATA.doc.full_doc
+        properties_node[
+            IN_DIRECTED_COLUMN_NAME
+        ] = NODE_COUNT_IN_EDGES_COLUMN_METADATA.doc.full_doc
+        properties_node[
+            IN_DIRECTED_MULTI_COLUMN_NAME
+        ] = NODE_COUNT_IN_EDGES_MULTI_COLUMN_METADATA.doc.full_doc
+        properties_node[
+            OUT_DIRECTED_COLUMN_NAME
+        ] = NODE_COUNT_OUT_EDGES_COLUMN_METADATA.doc.full_doc
+        properties_node[
+            OUT_DIRECTED_MULTI_COLUMN_NAME
+        ] = NODE_COUNT_OUT_EDGES_MULTI_COLUMN_METADATA.doc.full_doc
+
+        edge_properties_str = "\n\n".join(
+            f"***{key}***:\n\n{value}" for key, value in edge_properties.items()
+        )
+        node_properties_str = "\n\n".join(
+            f"***{key}***:\n\n{value}" for key, value in properties_node.items()
+        )
+
+        doc = cls.__doc__
+        doc_tables = f"""
+
+## Edges
+The 'edges' table contains the following columns:
+
+{edge_properties_str}
+
+## Nodes
+
+The 'nodes' table contains the following columns:
+
+{node_properties_str}
+
+"""
+
+        cls._cached_doc = f"{doc}\n\n{doc_tables}"
+        return cls._cached_doc
+
+    def parse_python_obj(self, data: Any) -> NetworkData:
+
+        if isinstance(data, KiaraTables):
+            if EDGES_TABLE_NAME not in data.tables.keys():
+                raise KiaraException(
+                    f"Can't import network data: no '{EDGES_TABLE_NAME}' table found"
+                )
+
+            if NODES_TABLE_NAME not in data.tables.keys():
+                raise KiaraException(
+                    f"Can't import network data: no '{NODES_TABLE_NAME}' table found"
+                )
+
+            # return NetworkData(
+            #     tables={
+            #         EDGES_TABLE_NAME: data.tables[EDGES_TABLE_NAME],
+            #         NODES_TABLE_NAME: data.tables[NODES_TABLE_NAME],
+            #     },
+            #
+            # )
+            return NetworkData.create_network_data(
+                edges_table=data.tables[EDGES_TABLE_NAME].arrow_table,
+                nodes_table=data.tables[NODES_TABLE_NAME].arrow_table,
+                augment_tables=False,
+            )
+
+        if not isinstance(data, NetworkData):
+            raise KiaraException(
+                f"Can't parse object to network data: invalid type '{type(data)}'."
+            )
+
+        return data
+
+    def _validate(cls, value: Any) -> None:
+        if not isinstance(value, NetworkData):
+            raise ValueError(
+                f"Invalid type '{type(value)}': must be of 'NetworkData' (or a sub-class)."
+            )
+
+        network_data: NetworkData = value
+
+        table_names = network_data.table_names
+        if EDGES_TABLE_NAME not in table_names:
+            raise Exception(
+                f"Invalid 'network_data' value: database does not contain table '{EDGES_TABLE_NAME}'."
+            )
+        if NODES_TABLE_NAME not in table_names:
+            raise Exception(
+                f"Invalid 'network_data' value: database does not contain table '{NODES_TABLE_NAME}'."
+            )
+
+        edges_columns = network_data.edges.column_names
+        if SOURCE_COLUMN_NAME not in edges_columns:
+            raise Exception(
+                f"Invalid 'network_data' value: 'edges' table does not contain a '{SOURCE_COLUMN_NAME}' column. Available columns: {', '.join(edges_columns)}."
+            )
+        if TARGET_COLUMN_NAME not in edges_columns:
+            raise Exception(
+                f"Invalid 'network_data' value: 'edges' table does not contain a '{TARGET_COLUMN_NAME}' column. Available columns: {', '.join(edges_columns)}."
+            )
+
+        nodes_columns = network_data.nodes.column_names
+        if NODE_ID_COLUMN_NAME not in nodes_columns:
+            raise Exception(
+                f"Invalid 'network_data' value: 'nodes' table does not contain a '{NODE_ID_COLUMN_NAME}' column. Available columns: {', '.join(nodes_columns)}."
+            )
+        if LABEL_COLUMN_NAME not in nodes_columns:
+            raise Exception(
+                f"Invalid 'network_data' value: 'nodes' table does not contain a '{LABEL_COLUMN_NAME}' column. Available columns: {', '.join(nodes_columns)}."
+            )
+
+    def pretty_print_as__terminal_renderable(
+        self, value: Value, render_config: Mapping[str, Any]
+    ) -> Any:
+
+        max_rows = render_config.get(
+            "max_no_rows", DEFAULT_PRETTY_PRINT_CONFIG["max_no_rows"]
+        )
+        max_row_height = render_config.get(
+            "max_row_height", DEFAULT_PRETTY_PRINT_CONFIG["max_row_height"]
+        )
+        max_cell_length = render_config.get(
+            "max_cell_length", DEFAULT_PRETTY_PRINT_CONFIG["max_cell_length"]
+        )
+
+        half_lines: Union[int, None] = None
+        if max_rows:
+            half_lines = int(max_rows / 2)
+
+        network_data: NetworkData = value.data
+
+        result: List[Any] = [""]
+
+        nodes_atw = ArrowTabularWrap(network_data.nodes.arrow_table)
+        nodes_pretty = nodes_atw.as_terminal_renderable(
+            rows_head=half_lines,
+            rows_tail=half_lines,
+            max_row_height=max_row_height,
+            max_cell_length=max_cell_length,
+        )
+        result.append(f"[b]{NODES_TABLE_NAME}[/b]")
+        result.append(nodes_pretty)
+
+        edges_atw = ArrowTabularWrap(network_data.edges.arrow_table)
+        edges_pretty = edges_atw.as_terminal_renderable(
+            rows_head=half_lines,
+            rows_tail=half_lines,
+            max_row_height=max_row_height,
+            max_cell_length=max_cell_length,
+        )
+        result.append(f"[b]{EDGES_TABLE_NAME}[/b]")
+        result.append(edges_pretty)
+
+        return Group(*result)
---
+++ src/kiara_plugin/network_analysis/utils.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+#  Copyright (c) 2022, Markus Binsteiner
+#
+#  Mozilla Public License, version 2.0 (see LICENSE or https://www.mozilla.org/en-US/MPL/2.0/)
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Dict,
+    Hashable,
+    Iterable,
+    List,
+    Tuple,
+    Union,
+)
+
+from kiara.exceptions import KiaraException
+from kiara_plugin.network_analysis.defaults import (
+    CONNECTIONS_COLUMN_NAME,
+    CONNECTIONS_MULTI_COLUMN_NAME,
+    COUNT_DIRECTED_COLUMN_NAME,
+    COUNT_IDX_DIRECTED_COLUMN_NAME,
+    COUNT_IDX_UNDIRECTED_COLUMN_NAME,
+    COUNT_UNDIRECTED_COLUMN_NAME,
+    EDGE_ID_COLUMN_NAME,
+    IN_DIRECTED_COLUMN_NAME,
+    IN_DIRECTED_MULTI_COLUMN_NAME,
+    LABEL_COLUMN_NAME,
+    NODE_ID_COLUMN_NAME,
+    OUT_DIRECTED_COLUMN_NAME,
+    OUT_DIRECTED_MULTI_COLUMN_NAME,
+    SOURCE_COLUMN_NAME,
+    TARGET_COLUMN_NAME,
+    UNWEIGHTED_DEGREE_CENTRALITY_COLUMN_NAME,
+    UNWEIGHTED_DEGREE_CENTRALITY_MULTI_COLUMN_NAME,
+)
+
+if TYPE_CHECKING:
+    import networkx as nx
+    import polars as pl
+    import pyarrow as pa
+    from sqlalchemy import MetaData, Table  # noqa
+
+
+def extract_networkx_nodes_as_table(
+    graph: "nx.Graph",
+    label_attr_name: Union[str, None, Iterable[str]] = None,
+    ignore_attributes: Union[None, Iterable[str]] = None,
+) -> Tuple["pa.Table", Dict[Hashable, int]]:
+    """Extract the nodes of a networkx graph as a pyarrow table.
+
+    Arguments:
+        graph: the networkx graph
+        label_attr_name: the name of the node attribute that should be used as label. If None, the node id is used.
+        ignore_attributes: a list of node attributes that should be ignored and not added to the table
+
+    Returns:
+        a tuple with the table and a map containing the original node id as key and the newly created internal node id (int) as value
+    """
+    # adapted from networx code
+    # License: 3-clause BSD license
+    # Copyright (C) 2004-2022, NetworkX Developers
+
+    import pyarrow as pa
+
+    # nan = float("nan")
+
+    nodes: Dict[str, List[Any]] = {
+        NODE_ID_COLUMN_NAME: [],
+        LABEL_COLUMN_NAME: [],
+    }
+    nodes_map = {}
+
+    for i, (node_id, node_data) in enumerate(graph.nodes(data=True)):
+        nodes[NODE_ID_COLUMN_NAME].append(i)
+        if label_attr_name is None:
+            nodes[LABEL_COLUMN_NAME].append(str(node_id))
+        elif isinstance(label_attr_name, str):
+            label = node_data.get(label_attr_name, None)
+            if label:
+                nodes[LABEL_COLUMN_NAME].append(str(label))
+            else:
+                nodes[LABEL_COLUMN_NAME].append(str(node_id))
+        else:
+            label_final = None
+            for label in label_attr_name:
+                label_final = node_data.get(label, None)
+                if label_final:
+                    break
+            if not label_final:
+                label_final = node_id
+            nodes[LABEL_COLUMN_NAME].append(str(label_final))
+
+        nodes_map[node_id] = i
+        for k in node_data.keys():
+            if ignore_attributes and k in ignore_attributes:
+                continue
+
+            if k.startswith("_"):
+                raise KiaraException(
+                    "Graph contains node column name starting with '_'. This is reserved for internal use, and not allowed."
+                )
+
+            v = node_data.get(k, None)
+            nodes.setdefault(k, []).append(v)
+
+    nodes_table = pa.Table.from_pydict(mapping=nodes)
+
+    return nodes_table, nodes_map
+
+
+def extract_networkx_edges_as_table(
+    graph: "nx.Graph", node_id_map: Dict[Hashable, int]
+) -> "pa.Table":
+    """Extract the edges of this graph as a pyarrow table.
+
+    The provided `node_id_map` might be modified if a node id is not yet in the map.
+
+    Args:
+        graph: The graph to extract edges from.
+        node_id_map: A mapping from (original) node ids to (kiara-internal) (integer) node-ids.
+    """
+
+    # adapted from networx code
+    # License: 3-clause BSD license
+    # Copyright (C) 2004-2022, NetworkX Developers
+
+    import pyarrow as pa
+
+    if node_id_map is None:
+        node_id_map = {}
+
+    # nan = float("nan")
+
+    max_node_id = max(node_id_map.values())  # TODO: could we just use len(node_id_map)?
+    edge_columns: Dict[str, List[int]] = {
+        SOURCE_COLUMN_NAME: [],
+        TARGET_COLUMN_NAME: [],
+    }
+
+    for source, target, edge_data in graph.edges(data=True):
+        if source not in node_id_map.keys():
+            max_node_id += 1
+            node_id_map[source] = max_node_id
+        if target not in node_id_map.keys():
+            max_node_id += 1
+            node_id_map[target] = max_node_id
+
+        edge_columns[SOURCE_COLUMN_NAME].append(node_id_map[source])
+        edge_columns[TARGET_COLUMN_NAME].append(node_id_map[target])
+
+        for k in edge_data.keys():
+            if k.startswith("_"):
+                raise KiaraException(
+                    "Graph contains edge column name starting with '_'. This is reserved for internal use, and not allowed."
+                )
+
+            v = edge_data.get(k, None)
+            edge_columns.setdefault(k, []).append(v)
+
+    edges_table = pa.Table.from_pydict(mapping=edge_columns)
+
+    return edges_table
+
+
+def augment_nodes_table_with_connection_counts(
+    nodes_table: Union["pa.Table", "pl.DataFrame"],
+    edges_table: Union["pa.Table", "pl.DataFrame"],
+) -> "pa.Table":
+
+    import duckdb
+
+    try:
+        nodes_column_names = nodes_table.column_names  # type: ignore
+    except Exception:
+        nodes_column_names = nodes_table.columns  # type: ignore
+
+    node_attr_columns = [x for x in nodes_column_names if not x.startswith("_")]
+    if node_attr_columns:
+        other_columns = ", " + ", ".join(node_attr_columns)
+    else:
+        other_columns = ""
+
+    # we can avoid 'COUNT(*)' calls in the following  query
+    nodes_table_rows = len(nodes_table)
+    print(nodes_table_rows)
+
+    query = f"""
+    SELECT
+         {NODE_ID_COLUMN_NAME},
+         {LABEL_COLUMN_NAME},
+         COALESCE(e1.{IN_DIRECTED_COLUMN_NAME}, 0) + COALESCE(e3.{OUT_DIRECTED_COLUMN_NAME}, 0) as {CONNECTIONS_COLUMN_NAME},
+         (COALESCE(e1._in_edges, 0) + COALESCE(e3._out_edges, 0)) / {nodes_table_rows} AS _degree_centrality,
+         COALESCE(e2.{IN_DIRECTED_MULTI_COLUMN_NAME}, 0) + COALESCE(e4.{OUT_DIRECTED_MULTI_COLUMN_NAME}, 0) as {CONNECTIONS_MULTI_COLUMN_NAME},
+         COALESCE(e1.{IN_DIRECTED_COLUMN_NAME}, 0) as {IN_DIRECTED_COLUMN_NAME},
+         COALESCE(e2.{IN_DIRECTED_MULTI_COLUMN_NAME}, 0) as {IN_DIRECTED_MULTI_COLUMN_NAME},
+         COALESCE(e3.{OUT_DIRECTED_COLUMN_NAME}, 0) as {OUT_DIRECTED_COLUMN_NAME},
+         COALESCE(e4.{OUT_DIRECTED_MULTI_COLUMN_NAME}, 0) as {OUT_DIRECTED_MULTI_COLUMN_NAME}
+         {other_columns}
+         FROM nodes_table n
+         left join
+           (SELECT {TARGET_COLUMN_NAME}, {COUNT_IDX_DIRECTED_COLUMN_NAME}, COUNT(*) as {IN_DIRECTED_COLUMN_NAME} from edges_table GROUP BY {TARGET_COLUMN_NAME}, {COUNT_IDX_DIRECTED_COLUMN_NAME}) e1
+           on n.{NODE_ID_COLUMN_NAME} = e1.{TARGET_COLUMN_NAME} and e1.{COUNT_IDX_DIRECTED_COLUMN_NAME} = 1
+         left join
+           (SELECT {TARGET_COLUMN_NAME}, COUNT(*) as {IN_DIRECTED_MULTI_COLUMN_NAME} from edges_table GROUP BY {TARGET_COLUMN_NAME}) e2
+           on n.{NODE_ID_COLUMN_NAME} = e2.{TARGET_COLUMN_NAME}
+         left join
+           (SELECT {SOURCE_COLUMN_NAME}, {COUNT_IDX_DIRECTED_COLUMN_NAME}, COUNT(*) as {OUT_DIRECTED_COLUMN_NAME} from edges_table GROUP BY {SOURCE_COLUMN_NAME}, {COUNT_IDX_DIRECTED_COLUMN_NAME}) e3
+           on n.{NODE_ID_COLUMN_NAME} = e3.{SOURCE_COLUMN_NAME} and e3.{COUNT_IDX_DIRECTED_COLUMN_NAME} = 1
+         left join
+           (SELECT {SOURCE_COLUMN_NAME}, COUNT(*) as {OUT_DIRECTED_MULTI_COLUMN_NAME} from edges_table GROUP BY {SOURCE_COLUMN_NAME}) e4
+           on n.{NODE_ID_COLUMN_NAME} = e4.{SOURCE_COLUMN_NAME}
+        ORDER BY {NODE_ID_COLUMN_NAME}
+    """
+
+    print(query)
+    nodes_table_result = duckdb.sql(query)
+
+    centrality_query = f"""
+    SELECT
+         {NODE_ID_COLUMN_NAME},
+         {LABEL_COLUMN_NAME},
+         {CONNECTIONS_COLUMN_NAME},
+         {CONNECTIONS_COLUMN_NAME} / (SELECT COUNT(*) FROM nodes_table_result) AS {UNWEIGHTED_DEGREE_CENTRALITY_COLUMN_NAME},
+         {CONNECTIONS_MULTI_COLUMN_NAME},
+         {CONNECTIONS_MULTI_COLUMN_NAME} / (SELECT COUNT(*) FROM nodes_table_result) AS {UNWEIGHTED_DEGREE_CENTRALITY_MULTI_COLUMN_NAME},
+         {IN_DIRECTED_COLUMN_NAME},
+         {IN_DIRECTED_MULTI_COLUMN_NAME},
+         {OUT_DIRECTED_COLUMN_NAME},
+         {OUT_DIRECTED_MULTI_COLUMN_NAME}
+         {other_columns}
+    FROM nodes_table_result
+    """
+    print(centrality_query)
+
+    result = duckdb.sql(centrality_query)
+
+    nodes_table_augmented = result.arrow()
+    return nodes_table_augmented
+
+
+def augment_edges_table_with_id_and_weights(
+    edges_table: Union["pa.Table", "pl.DataFrame"]
+) -> "pa.Table":
+    """Augment the edges table with additional pre-computed columns for directed and undirected weights.."""
+
+    import duckdb
+
+    try:
+        column_names = edges_table.column_names  # type: ignore
+    except Exception:
+        column_names = edges_table.columns  # type: ignore
+
+    edge_attr_columns = [x for x in column_names if not x.startswith("_")]
+    if edge_attr_columns:
+        other_columns = ", " + ", ".join(edge_attr_columns)
+    else:
+        other_columns = ""
+
+    query = f"""
+    SELECT
+      ROW_NUMBER() OVER () -1 as {EDGE_ID_COLUMN_NAME},
+      {SOURCE_COLUMN_NAME},
+      {TARGET_COLUMN_NAME},
+      COUNT(*) OVER (PARTITION BY {SOURCE_COLUMN_NAME}, {TARGET_COLUMN_NAME}) as {COUNT_DIRECTED_COLUMN_NAME},
+      ROW_NUMBER(*) OVER (PARTITION BY {SOURCE_COLUMN_NAME}, {TARGET_COLUMN_NAME}) as {COUNT_IDX_DIRECTED_COLUMN_NAME},
+      COUNT(*) OVER (PARTITION BY LEAST({SOURCE_COLUMN_NAME}, {TARGET_COLUMN_NAME}), GREATEST({SOURCE_COLUMN_NAME}, {TARGET_COLUMN_NAME})) as {COUNT_UNDIRECTED_COLUMN_NAME},
+      ROW_NUMBER(*) OVER (PARTITION BY LEAST({SOURCE_COLUMN_NAME}, {TARGET_COLUMN_NAME}), GREATEST({SOURCE_COLUMN_NAME}, {TARGET_COLUMN_NAME})) as {COUNT_IDX_UNDIRECTED_COLUMN_NAME}
+      {other_columns}
+    FROM edges_table"""
+
+    result = duckdb.sql(query)
+    edges_table_augmented = result.arrow()
+
+    return edges_table_augmented
